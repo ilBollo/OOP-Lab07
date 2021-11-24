767626ae978f32ddede766803c95a25e916b91e8
Simone Bollini <simone.bollini@studio.unibo.it>
2021-11-24 09:18:14 +0100

    finished exercise enum1

--- src/it/unibo/oop/lab/enum1/SportSocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/enum1/SportSocialNetworkUserImpl.java
@@ -3,6 +3,9 @@
  */
 package it.unibo.oop.lab.enum1;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUserImpl;
 import it.unibo.oop.lab.socialnetwork.User;
 
@@ -25,13 +28,10 @@ import it.unibo.oop.lab.socialnetwork.User;
  */
 public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUserImpl<U> {
 
-    /*
-     * TODO
-     * 
-     * add a field to keep track of the set of sports followed/done by a user
-     */
-
-    /**
+	 
+	private final Set<Sport> sports;
+	
+	/**
      * Builds a new {@link SportSocialNetworkUserImpl}.
      * 
      * @param name
@@ -59,25 +59,14 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      *            alias of the user, i.e. the way a user is identified on an
      *            application
      */
-    public SportSocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
-        super(name, surname, user, userAge);
+    public SportSocialNetworkUserImpl(final String name, final String surname,
+    									final String user, final int userAge) {
+		super(name, surname, user, userAge);
+    	this.sports = new HashSet<>();
     }
 
-    /*
-     * [METHODS]
-     * 
-     * Redefine the methods below reusing the same code of previous exercise
-     */
-
-    /**
-     * Add a new sport followed by this user: if the user already likes or does
-     * the sport, nothing happens.
-     * 
-     * @param sport
-     *            a sport followed/done by the user
-     */
-    // TODO
     public void addSport(final Sport sport) {
+		this.sports.add(sport);
 
     }
 
@@ -86,10 +75,10 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      * 
      * @param s
      *            sport to use as an input
-     * 
-     * @return true if the user likes sport s
+     * @return true if a user likes sport s
      */
     public boolean hasSport(final Sport s) {
-        return false;
+        return this.sports.contains(s);
     }
+       	 
 }
